ceb0fa6af6a8742dbeb229b8826fff645fef8684
xavi <xaviw@foxmail.com>
2023-08-15 18:11:55 +0800

    feat: 支持从网络获取源码

--- bin/index.js
+++ bin/index.js
@@ -14,77 +14,152 @@ const spinner = ora()
 program.version('1.0.0', '-v, --version')
 
 program.command('create', { isDefault: true }).action(async () => {
-  const { targetPath, url, originPath, inline } = await inquirer.prompt([
+  const { targetPath, fontUrl, inline } = await inquirer.prompt([
     {
       name: 'targetPath',
       message: '组件创建路径以及组件名',
       default: './components/CustomIcon',
     },
     {
-      name: 'url',
-      message: '字体文件路径(本地或网络路径)',
-    },
-    {
-      name: 'originPath',
-      message: 'u-icon源码路径',
-      default: './uni_modules/uview-ui/components/u-icon',
+      name: 'fontUrl',
+      message: 'ttf字体文件路径(本地或网络路径)',
     },
     {
       name: 'inline',
-      message: '是否以Base64格式内嵌',
+      message: '是否以Base64格式内嵌(默认N)',
       type: 'confirm',
       default: false,
     },
   ])
 
-  let networkUrl
-  let fontBuffer
-  let fontType
-  let chartMap = {}
-  let formatType
-  let fontFamily
-
-  // 读取字体文件
+  // 解析字体链接
+  let ext, url
   try {
-    if (/^(http|https)?(\:)?\/\/.*?$/i.test(url)) {
-      // 如果是网络地址
-      // 补充完整以"//"开头的链接（//at.alicdn.com/t/c/font_4200663_ne0mnk9sfr.ttf?t=1691456853251）
-      networkUrl = url
-      if (!url.startsWith('http')) {
-        const index = url.indexOf('/')
-        networkUrl = `https:${url.slice(index)}`.replace(/\?.*?$/, '')
-      }
-      spinner.text = '下载字体中...'
-      spinner.start()
-      const response = await fetch(networkUrl)
-      fontBuffer = await response.arrayBuffer()
-      spinner.succeed('字体下载完成')
-    } else {
-      // 是本地地址
-      const fontExists = await fileExists(url)
-      if (fontExists !== true) {
-        console.log(fontExists)
-        return
-      }
-      spinner.text = '读取字体中...'
-      spinner.start()
-      fontBuffer = await fs.readFile(url)
-      spinner.succeed('字体读取成功')
-    }
-
-    // 解析字体类型
-    const ext = path.extname(url)
-    fontType = ext.match(/^\.(.*?)(?:\?.*?)?$/)?.[1]?.toLowerCase()
-    formatType = fontType === 'ttf' ? 'truetype' : fontType
-    if (!fontType || !['ttf', 'woff', 'woff2'].includes(fontType)) {
-      console.log(chalk.red(`字体文件扩展名解析错误，支持ttf、woff、woff2，解析值为${ext}`))
+    const fileInfo = await parseUrl(fontUrl)
+    ext = fileInfo.ext
+    url = fileInfo.url
+    if (ext !== 'ttf') {
+      console.log(chalk.red(`仅支持.ttf格式文件`))
       return
     }
+  } catch (error) {
+    return error
+  }
 
-    // 解析字体
-    spinner.text = '字体解析中...'
+  // 读取字体文件
+  let fontBuffer, fontFamily, chartMap
+  spinner.text = '开始读取字体'
+  spinner.start()
+  try {
+    fontBuffer = await getFontBuffer(url)
+    // 解析字体文件
+    const fontData = parseTTF(fontBuffer)
+    fontFamily = fontData.fontFamily
+    chartMap = fontData.chartMap
+  } catch (error) {
+    spinner.fail('字体读取失败')
+    return
+  }
+  spinner.succeed('字体解析成功')
+
+  // 保存组件代码
+  try {
+    spinner.text = `开始保存${targetPath}`
     spinner.start()
-    const font = Font.create(fontBuffer, { type: 'ttf' })
+    const name = path.parse(targetPath).name
+    let vueCode = await getUIconSourceCode('u-icon.vue')
+    vueCode = transformUIcon({ source: vueCode, inline, fontBuffer, fontFamily, fontUrl: url, name })
+    await fs.outputFile(targetPath, vueCode)
+    spinner.succeed(`${targetPath}保存成功`)
+  } catch (error) {
+    spinner.fail(`${targetPath}保存失败`)
+    return
+  }
+
+  // 保存props.js
+  const propsPath = path.join(targetPath, '../props.js')
+  try {
+    spinner.text = `开始保存${propsPath}`
+    spinner.start()
+    const propsCode = await getUIconSourceCode('props.js')
+    await fs.outputFile(propsPath, propsCode)
+    spinner.succeed(`${propsPath}保存成功`)
+  } catch (error) {
+    spinner.fail(`${propsPath}保存失败`)
+    return
+  }
+
+  // 保存icons.js
+  const iconsPath = path.join(targetPath, '../icons.js')
+  try {
+    spinner.text = `开始保存${iconsPath}`
+    spinner.start()
+    const iconsCode = transformIcons(chartMap)
+    await fs.outputFile(iconsPath, iconsCode)
+    spinner.succeed(`${iconsPath}保存成功`)
+  } catch (error) {
+    spinner.fail(`${iconsPath}保存失败`)
+    return
+  }
+})
+
+program.parse()
+
+// 检查文件是否存在
+function fileExists(path) {
+  return new Promise((resolve, reject) => {
+    fs.pathExists(path)
+      .then((flag) => {
+        if (!flag) reject(chalk.red(`文件"${path}"不存在`))
+        resolve()
+      })
+      .catch((err) => {
+        reject(chalk.red(`文件"${path}"检查错误：${err}`))
+      })
+  })
+}
+
+// 接收链接解析出扩展名与可用链接（本地路径会判断文件是否存在）
+async function parseUrl(originUrl) {
+  const ext = path
+    .extname(originUrl)
+    ?.match(/^\.(.*?)(?:\?.*?)?$/)?.[1]
+    ?.toLowerCase()
+  let url = originUrl
+
+  // 是网络路径
+  if (/^(http|https)?(\:)?\/\/.*?$/i.test(originUrl)) {
+    // 处理"//"开头的情况（//at.alicdn.com/t/c/xxx.ttf）
+    if (!originUrl.startsWith('http')) {
+      const index = originUrl.indexOf('/')
+      url = `https:${originUrl.slice(index)}`.replace(/\?.*?$/, '')
+    }
+  } else {
+    // 非网络路径，直接判断本地是否存在
+    try {
+      await fileExists(originUrl)
+    } catch (error) {
+      throw error
+    }
+  }
+
+  return { ext, url }
+}
+
+// 接收链接返回文件buffer
+async function getFontBuffer(url) {
+  if (url.startsWith('http')) {
+    return fetch(url).then((res) => res.arrayBuffer())
+  } else {
+    return fs.readFile(url)
+  }
+}
+
+// 解析ttf文件fontFamily与字符名称与Unicode编码对应表
+function parseTTF(buffer) {
+  try {
+    const chartMap = {}
+    const font = Font.create(buffer, { type: 'ttf' })
     fontFamily = font.data.name.fontFamily
     // 存储图标名称和编码
     font.data.glyf.forEach((glyph) => {
@@ -92,79 +167,59 @@ program.command('create', { isDefault: true }).action(async () => {
         chartMap[glyph.name] = `\\u${glyph.unicode[0].toString(16)}`
       }
     })
-    spinner.succeed('字体解析完成')
+    return { fontFamily, chartMap }
   } catch (error) {
-    spinner.fail(`文件读取失败：${error}`)
-    return
+    throw error
   }
+}
 
-  // 处理u-icon源码
-  try {
-    // 处理vue文件
-    const vuePath = originPath + '/u-icon.vue'
-    const vueExists = await fileExists(vuePath)
-    if (vueExists !== true) {
-      console.log(vueExists)
-      return
-    }
-    const compName = targetPath.split('/').pop()
-    spinner.text = '保存Vue代码...'
-    spinner.start()
-    let vueCode = await fs.readFile(vuePath, 'utf-8')
-    // 通用修改部分
-    vueCode = vueCode.replace(/['"]fontFamily['"]:\s*['"]uicon-iconfont['"]/i, `'fontFamily': '${fontFamily}'`)
-    vueCode = vueCode.replace(/font-family:\s*['"]uicon-iconfont['"]/i, `font-family: '${fontFamily}'`)
-    vueCode = vueCode.replace(/font-family:\s*uicon-iconfont/i, `font-family: ${fontFamily}`)
-    vueCode = vueCode.replace(/name: ['"]u-icon['"]/i, `name: '${compName}'`)
-    vueCode = vueCode.replace(/return icons\[['"]uicon-['"]\s*\+\s*this.name\]/i, `return icons[this.name]`)
-    vueCode = vueCode.replace(/\@import\s*.*?;/i, `@import "@/uni_modules/uview-ui/libs/css/components.scss";`)
-    // 字体引入部分
-    if (inline) {
-      const base64 = `data:application/font-${fontType};charset=utf-8;base64,` + Buffer.from(fontBuffer).toString('base64')
-      vueCode = vueCode.replace(/['"]?src['"]?:\s*`url\('\$\{fontUrl\}'\)`/i, `'src': 'url("${base64}")'`)
-      vueCode = vueCode.replace(/['"]?src['"]?:\s*url\('.*?'\)\s*format\('truetype'\)\;/i, `src: url('${base64}')`)
-    } else {
-      vueCode = vueCode.replace(/const\s+fontUrl\s+=\s+'.*?'/i, `const fontUrl = '${networkUrl || url}'`)
-      vueCode = vueCode.replace(/src: url\('.*?'\)\s*format\('truetype'\);/i, `src: url('${networkUrl || url}') format('${formatType}');`)
-    }
-    fs.outputFile(`${targetPath}/${compName}.vue`, vueCode)
-    spinner.succeed(`${targetPath}/${compName}.vue已保存`)
+// 获取u-icon文件源码（u-icon.vue或props.js）
+async function getUIconSourceCode(fileName) {
+  const uniPath = './uni_modules/uview-ui/components/u-icon/' + fileName
+  const nodePath = './node_modules/uview-ui/components/u-icon/' + fileName
+  const networkPath = 'https://raw.githubusercontent.com/umicro/uView2.0/master/uni_modules/uview-ui/components/u-icon/' + fileName
+  let url
 
-    // 处理props文件
-    const propsPath = originPath + '/props.js'
-    const propsExists = await fileExists(propsPath)
-    if (propsExists !== true) {
-      console.log(propsExists)
-      return
-    }
-    spinner.text = '保存props代码...'
-    spinner.start()
-    const propsCode = await fs.readFile(propsPath, 'utf-8')
-    fs.outputFile(`${targetPath}/props.js`, propsCode)
-    spinner.succeed(`${targetPath}/props.js已保存`)
+  // 判断本地源码文件是否存在
+  await fileExists(uniPath)
+    .then(() => {
+      url = uniPath
+    })
+    .catch(() => {
+      return fileExists(nodePath).then(() => {
+        url = nodePath
+      })
+    })
 
-    // 处理icons.js
-    spinner.text = '保存icons代码...'
-    spinner.start()
-    const iconsCode = `export default ${JSON.stringify(chartMap)}`.replace(/\\\\/g, '\\')
-    fs.outputFile(`${targetPath}/icons.js`, iconsCode)
-    spinner.succeed(`${targetPath}/icons.js已保存`)
-  } catch (error) {
-    spinner.fail(`保存失败：${error}`)
-    return
+  if (url) {
+    return fs.readFile(url, 'utf-8')
+  } else {
+    return fetch(networkPath).then((res) => res.text())
   }
-})
+}
 
-program.parse()
+// 处理u-icon.vue文件
+function transformUIcon({ source, inline, fontFamily, name, fontBuffer, fontUrl }) {
+  // 通用修改部分
+  source = source.replace(/['"]fontFamily['"]:\s*['"]uicon-iconfont['"]/i, `'fontFamily': '${fontFamily}'`)
+  source = source.replace(/font-family:\s*['"]uicon-iconfont['"]/i, `font-family: '${fontFamily}'`)
+  source = source.replace(/font-family:\s*uicon-iconfont/i, `font-family: ${fontFamily}`)
+  source = source.replace(/name: ['"]u-icon['"]/i, `name: '${name}'`)
+  source = source.replace(/return icons\[['"]uicon-['"]\s*\+\s*this.name\]/i, `return icons[this.name]`)
+  source = source.replace(/\@import\s*.*?;/i, `@import "@/uni_modules/uview-ui/libs/css/components.scss";`)
+  // 字体引入部分
+  if (inline) {
+    const base64 = `data:application/font-ttf;charset=utf-8;base64,` + Buffer.from(fontBuffer).toString('base64')
+    source = source.replace(/['"]?src['"]?:\s*`url\('\$\{fontUrl\}'\)`/i, `'src': 'url("${base64}")'`)
+    source = source.replace(/['"]?src['"]?:\s*url\('.*?'\)\s*format\('truetype'\)\;/i, `src: url('${base64}')`)
+  } else {
+    source = source.replace(/const\s+fontUrl\s+=\s+'.*?'/i, `const fontUrl = '${fontUrl}'`)
+    source = source.replace(/src: url\('.*?'\)\s*format\('truetype'\);/i, `src: url('${fontUrl}') format('truetype');`)
+  }
+  return source
+}
 
-function fileExists(path) {
-  return fs
-    .pathExists(path)
-    .then((flag) => {
-      if (!flag) return chalk.red(`文件"${path}"不存在`)
-      return true
-    })
-    .catch((err) => {
-      return chalk.red(`文件"${path}"检查错误：${err}`)
-    })
+// 处理props.js文件
+function transformIcons(chartMap) {
+  return `export default ${JSON.stringify(chartMap)}`.replace(/\\\\/g, '\\')
 }
